e1314ff763fa0593dbda106586a9f09d5bb2af6c
Shadetamic <141822385+Shadetamic@users.noreply.github.com>
2023-08-22 16:31:40 +0200

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 3956818381 then -- NINJA LEGENDS
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Shadetamic/horizon/main/NinjaLegends"))()
+else
+  game.Players.LocalPlayer:Kick("horizon.cc | this game is currently unsupported, for more information, visit https://discord.gg/hmApmtj2td")
 end
