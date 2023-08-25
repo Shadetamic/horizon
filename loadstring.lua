3e59bf40b0547770929a755c050640d917302d05
Shadetamic <141822385+Shadetamic@users.noreply.github.com>
2023-08-25 13:59:33 +0200

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,7 +1,11 @@
 print("testing.")
 if game.PlaceId == 3956818381 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Shadetamic/horizon/main/NinjaLegends"))()
-else
-print("testetetete")
+else if game.PlaceId == 14328009401 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Shadetamic/horizon/main/NinjaLegends"))()
+  else
+    game.Players.LocalPlayer:Kick("Unsupported game.")
+  end
+
 end
 print("hi"9
