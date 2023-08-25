3cd83c063800754210e842e9eaeef3a1bea20e36
Shadetamic <141822385+Shadetamic@users.noreply.github.com>
2023-08-25 13:48:49 +0200

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,8 +1,5 @@
 if game.PlaceId == 3956818381 then -- NINJA LEGENDS
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Shadetamic/horizon/main/NinjaLegends"))()
-else if game.PlaceId == 14328009401 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/Shadetamic/horizon/main/BlockMayhem"))()
 else
   game.Players.LocalPlayer:Kick("horizon.cc | this game is currently unsupported, for more information, visit https://discord.gg/hmApmtj2td")
-end
 end
